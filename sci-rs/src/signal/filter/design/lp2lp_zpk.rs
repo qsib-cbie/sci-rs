a7a0dfdaaef3d5d9e47a84d73663f2193952c9da
laurasfelix <laurasfelix@hotmail.com.br>
2023-06-12 10:58:48 -0500

    finished trying to implement low pass, adding tests now

--- sci-rs/src/signal/filter/design/lp2lp_zpk.rs
+++ sci-rs/src/signal/filter/design/lp2lp_zpk.rs
@@ -9,63 +9,74 @@ use super::{
 };
 
 
-    // Transform a lowpass filter prototype to a different frequency.
+    /// Transform a lowpass filter prototype to a different frequency.
 
-    // Return an analog low-pass filter with cutoff frequency `wo`
-    // from an analog low-pass filter prototype with unity cutoff frequency,
-    // using zeros, poles, and gain ('zpk') representation.
+    /// Return an analog low-pass filter with cutoff frequency `wo`
+    /// from an analog low-pass filter prototype with unity cutoff frequency,
+    /// using zeros, poles, and gain ('zpk') representation.
 
-    // Parameters
-    // ----------
-    // z : array_like
-    //     Zeros of the analog filter transfer function.
-    // p : array_like
-    //     Poles of the analog filter transfer function.
-    // k : float
-    //     System gain of the analog filter transfer function.
-    // wo : float
-    //     Desired cutoff, as angular frequency (e.g., rad/s).
-    //     Defaults to no change.
+    /// Parameters
+    /// ----------
+    /// z : array_like
+    ///     Zeros of the analog filter transfer function.
+    /// p : array_like
+    ///     Poles of the analog filter transfer function.
+    /// k : float
+    ///     System gain of the analog filter transfer function.
+    /// wo : float
+    ///     Desired cutoff, as angular frequency (e.g., rad/s).
+    ///     Defaults to no change.
 
-    // Returns
-    // -------
-    // z : ndarray
-    //     Zeros of the transformed low-pass filter transfer function.
-    // p : ndarray
-    //     Poles of the transformed low-pass filter transfer function.
-    // k : float
-    //     System gain of the transformed low-pass filter.
+    /// Returns
+    /// -------
+    /// z : ndarray
+    ///     Zeros of the transformed low-pass filter transfer function.
+    /// p : ndarray
+    ///     Poles of the transformed low-pass filter transfer function.
+    /// k : float
+    ///     System gain of the transformed low-pass filter.
 
-    // See Also
-    // --------
-    // lp2hp_zpk, lp2bp_zpk, lp2bs_zpk, bilinear
-    // lp2lp
+    /// See Also
+    /// --------
+    /// lp2hp_zpk, lp2bp_zpk, lp2bs_zpk, bilinear
+    /// lp2lp
 
-    // Notes
-    // -----
-    // This is derived from the s-plane substitution
+    /// Notes
+    /// -----
+    /// This is derived from the s-plane substitution
 
-    // .. math:: s \rightarrow \frac{s}{\omega_0}
+    /// .. math:: s \rightarrow \frac{s}{\omega_0}
 
-    // .. versionadded:: 1.1.0
+    /// .. versionadded:: 1.1.0
 
 
 #[cfg(feature = "use_std")]
-pub fn lp2lp_zpk_dyn<F>(zpk: ZpkFormatFilter<F>, wo: Option<F>, bw: Option<F>) -> ZpkFormatFilter<F>
+pub fn lp2lp_zpk_dyn<F>(zpk: ZpkFormatFilter<F>, wo: Option<F>) -> ZpkFormatFilter<F>
 where
     F: RealField + Float,
 {
-    let wo = wo.unwrap_or_else(F::one);  // Avoid int wraparound
+    let wo = wo.unwrap_or_else(F::one); // Avoid int wraparound
 
     let degree = relative_degree_dyn(&zpk.z, &zpk.p);
 
-    // Scale all points radially from origin to shift cutoff frequency
-    z_lp = wo * z;
-    p_lp = wo * p;
+    /// Scale all points radially from origin to shift cutoff frequency
+    let z_lp: Vec<_> = zpk
+        .z
+        .iter()
+        .map(|zi| *zi * Complex::new(wo, F::zero()))
+        .collect();
 
-    // Each shifted pole decreases gain by wo, each shifted zero increases it.
-    // Cancel out the net change to keep overall gain the same
-    k_lp = k * wo**degree;
+    let p_lp: Vec<_> = zpk
+        .p
+        .iter()
+        .map(|zi| *zi * Complex::new(wo, F::zero()))
+        .collect();
 
-    ZpkFormatFilter::new(z_bp, p_bp, k_bp)
+
+    /// Each shifted pole decreases gain by wo, each shifted zero increases it.
+    /// Cancel out the net change to keep overall gain the same
+
+    let k_lp = zpk.k * Float::powi(wo, degree as i32);
+
+    ZpkFormatFilter::new(z_lp, p_lp, k_lp)
 } fn lp2lp_zpk_dyn
