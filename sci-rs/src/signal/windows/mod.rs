3f47ee9df7ff63a05de5eb8b9f9d3d010a62fae0
SpookyYomo <48710653+SpookyYomo@users.noreply.github.com>
2024-10-22 22:23:05 +0800

    Add signal::windows namespace and helper functions
    To implement firwin, there is a need to get_windows on all window types
    defined in the Scipy signal.windows namespace, most of which are
    implemented in a _windows.py file. This commit keeps all the window
    variants that needs to be implemented as comments in an Enum for future
    implementation, along with some functions that are used across the
    implementation of all window types.
    
    The intended strategy to combat the variadic function type associated to
    the Scipy's get_windows function is to enclose all function arguments in
    a struct that represents the given function type, while ensuring all
    structs representing the Windows type implement a common trait.

---
+++ sci-rs/src/signal/windows/mod.rs
@@ -0,0 +1,92 @@
+#[cfg(feature = "alloc")]
+use alloc::vec::Vec;
+use num_traits::{real::Real, Float};
+
+/// Corresponding window representation for tuple-structs of [Window] variants.
+#[cfg(feature = "alloc")]
+pub trait GetWindow<W = f64>
+where
+    W: Real,
+{
+    /// Returns a window of given length and type.
+    ///
+    /// # Parameters
+    /// `self`:  
+    ///     The type of window to construct, typically consists of at least the following
+    ///     arguments:
+    /// * `Nx`: usize  
+    ///     The number of samples in the window
+    /// * `fftbins/~sym`: bool  
+    ///     If fftbins=true/sym=false, create a “periodic” window, ready to use with ifftshift and
+    ///     be multiplied by the result of an FFT. This is the default behaviour in If
+    ///     fftbins=false/sym=true, create a "symmetric" window, for use in filter design.
+    /// * `*args`:  
+    ///     Other arguments relevant to the window type.
+    ///     
+    /// # Reference
+    /// <https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.windows.get_window.html>
+    #[cfg(feature = "alloc")]
+    fn get_window(&self) -> Vec<W>;
+}
+
+/// Private function for windows implementing [GetWindow]
+/// Handle small or incorrect window lengths.
+#[inline(always)]
+fn len_guard(m: usize) -> bool {
+    m <= 1
+}
+
+/// Private function for windows implementing [GetWindow]
+/// Extend window by 1 sample if needed for DFT-even symmetry.
+#[inline(always)]
+fn extend(m: usize, sym: bool) -> (usize, bool) {
+    if !sym {
+        (m + 1, true)
+    } else {
+        (m, false)
+    }
+}
+
+/// Private function for windows implementing [GetWindow]
+/// Truncate window by 1 sample if needed for DFT-even symmetry.
+#[inline(always)]
+fn truncate<W>(mut w: Vec<W>, needed: bool) -> Vec<W> {
+    if needed {
+        w.pop();
+    }
+    w
+}
+
+/// todo
+// Ordering is as in accordance with
+// https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.get_window.html.
+#[derive(Debug, Clone, PartialEq)]
+pub enum Window {
+    // Boxcar,
+    // Triangle,
+    // Blackman,
+    // Hamming,
+    // Hann,
+    // Bartlett,
+    // Flattop,
+    // Parzen,
+    // Bohman,
+    // BlackmanHarris,
+    // Nuttall,
+    // BartHann,
+    // Cosine,
+    // Exponential,
+    // Tukey,
+    // Taylor,
+    // Lanczos,
+    // Kaiser Window.
+    // Kaiser, // Needs Beta
+    // KaiserBesselDerived, // Needs Beta
+    // Gaussian, // Needs Standard Deviation
+    // Generic weighted sum of cosine term windows.
+    // GenericCosine(GenericCosine<T>), // Needs Weighting Coefficients
+    // GeneralGaussian, // Needs Power, Width
+    // GeneralHamming, // Needs Window Coefficients.
+    // Dpss, // Needs Normalized Half-Bandwidth.
+    // Chebwin, // Needs Attenuation.
+}
