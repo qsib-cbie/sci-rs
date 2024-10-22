0f4cfb04010689295fe6ac966106abe7449c1655
SpookyYomo <48710653+SpookyYomo@users.noreply.github.com>
2024-10-22 22:23:05 +0800

    Add signal::windows namespace and helper functions
    To implement firwin, there is a need to get_windows on all window types
    defined in the Scipy signal.windows namespace, most of which are
    implemented in a _windows.py file. This commit keeps all the window
    variants that needs to be implemented as comments in an Enum for future
    implementation, along with some functions that are used across the
    implementation of all window types.
    
    The intended strategy to combat the variadic function type associated to
    the Scipy's get_windows function is to enclose all function arguments in
    a struct that represents the given function type, while ensuring all
    structs representing the Windows type implement a common trait.

--- sci-rs/src/signal/mod.rs
+++ sci-rs/src/signal/mod.rs
@@ -8,6 +8,16 @@ pub mod wave;
 #[cfg(feature = "std")]
 pub mod convolve;
 
+/// Window functions  
+/// This contains all window functions in the
+/// [scipy.signal.windows](https://docs.scipy.org/doc/scipy/reference/signal.windows.html#module-scipy.signal.windows)
+/// namespace.  
+/// The convenience function
+/// [`get_windows`](https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.get_window.html#scipy.signal.get_window)
+/// in the [scipy.signal](https://docs.scipy.org/doc/scipy/reference/signal.html#window-functions)
+/// namespace is located here.
+pub mod windows;
+
 /// Signal Resampling
 #[cfg(feature = "std")]
 pub mod resample;
