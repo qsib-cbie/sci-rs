9f062fd6cc9a9583920894b7c20694068fd428f2
SpookyYomo <48710653+SpookyYomo@users.noreply.github.com>
2025-02-06 06:40:41 +0800

    Add signal::cheby2

--- sci-rs/src/signal/filter/design/iirfilter.rs
+++ sci-rs/src/signal/filter/design/iirfilter.rs
@@ -440,6 +440,90 @@ where
     ZpkFormatFilter { z, p, k }
 }
 
+/// Chebyshev type II digital and analog filter design.
+///
+/// Design an Nth-order digital or analog Chebyshev type II filter and
+/// return the filter coefficients.
+///
+/// Parameters
+/// ----------
+/// * `N` : int  
+///     The order of the filter.
+/// * `rs` : float  
+///     The minimum attenuation required in the stop band.
+///     Specified in decibels, as a positive number.
+/// * `Wn` : array_like  
+///     A scalar or length-2 sequence giving the critical frequencies.
+///     For Type II filters, this is the point in the transition band at which
+///     the gain first reaches -`rs`.
+///
+///     For digital filters, `Wn` are in the same units as `fs`. By default,
+///     `fs` is 2 half-cycles/sample, so these are normalized from 0 to 1,
+///     where 1 is the Nyquist frequency. (`Wn` is thus in
+///     half-cycles / sample.)
+///
+///     For analog filters, `Wn` is an angular frequency (e.g., rad/s).
+/// * `btype` : {'lowpass', 'highpass', 'bandpass', 'bandstop'}, optional  
+///     The type of filter.  Default is 'lowpass'.
+/// * `analog` : bool, optional  
+///     When True, return an analog filter, otherwise a digital filter is
+///     returned.
+/// * `output` : {'ba', 'zpk', 'sos'}, optional  
+///     Type of output:  numerator/denominator ('ba'), pole-zero ('zpk'), or
+///     second-order sections ('sos'). Default is 'ba' for backwards
+///     compatibility, but 'sos' should be used for general-purpose filtering.
+/// * `fs` : float, optional  
+///     The sampling frequency of the digital system.
+///
+/// Returns
+/// -------
+/// b, a : ndarray, ndarray
+///     Numerator (`b`) and denominator (`a`) polynomials of the IIR filter.
+///     Only returned if ``output='ba'``.
+/// z, p, k : ndarray, ndarray, float
+///     Zeros, poles, and system gain of the IIR filter transfer
+///     function.  Only returned if ``output='zpk'``.
+/// sos : ndarray
+///     Second-order sections representation of the IIR filter.
+///     Only returned if ``output='sos'``.
+///
+/// See Also
+/// --------
+/// cheb2ord, [cheb2ap_dyn]
+///
+/// Notes
+/// -----
+/// The Chebyshev type II filter maximizes the rate of cutoff between the
+/// frequency response's passband and stopband, at the expense of ripple in
+/// the stopband and increased ringing in the step response.
+///
+/// Type II filters do not roll off as fast as Type I (`cheby1`).
+#[cfg(feature = "alloc")]
+pub fn cheby2_dyn<F>(
+    n: usize,
+    rs: F,
+    wn: Vec<F>,
+    btype: Option<FilterBandType>,
+    analog: Option<bool>,
+    output: Option<FilterOutputType>,
+    fs: Option<F>,
+) -> DigitalFilter<F>
+where
+    F: RealField + Float + Sum,
+{
+    iirfilter_dyn(
+        n,
+        wn,
+        None, // rp
+        Some(rs),
+        btype,
+        Some(FilterType::ChebyshevII),
+        analog,
+        output,
+        fs,
+    )
+}
+
 #[cfg(test)]
 mod tests {
     use approx::assert_relative_eq;
