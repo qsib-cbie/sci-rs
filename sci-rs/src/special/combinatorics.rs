4d4e32019804ec09e29ca747ae4e46f582674924
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2025-02-11 23:18:38 -0600

    fix cargo fmt

--- sci-rs/src/special/combinatorics.rs
+++ sci-rs/src/special/combinatorics.rs
@@ -187,9 +187,9 @@ mod tests {
     use super::*;
     use core::fmt;
 
-    fn check_values<K, T>(ref_values: &[[K;10]], func: fn(T, T) -> T)
+    fn check_values<K, T>(ref_values: &[[K; 10]], func: fn(T, T) -> T)
     where
-        K: PrimInt + FromPrimitive, 
+        K: PrimInt + FromPrimitive,
         T: PrimInt + FromPrimitive + fmt::Debug,
     {
         for (n, &elements) in ref_values.iter().enumerate() {
@@ -206,16 +206,16 @@ mod tests {
     fn comb() {
         // Generated from scipy
         let ref_values = [
-           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-           [1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-           [1, 2, 1, 0, 0, 0, 0, 0, 0, 0],
-           [1, 3, 3, 1, 0, 0, 0, 0, 0, 0],
-           [1, 4, 6, 4, 1, 0, 0, 0, 0, 0],
-           [1, 5, 10, 10, 5, 1, 0, 0, 0, 0],
-           [1, 6, 15, 20, 15, 6, 1, 0, 0, 0],
-           [1, 7, 21, 35, 35, 21, 7, 1, 0, 0],
-           [1, 8, 28, 56, 70, 56, 28, 8, 1, 0],
-           [1, 9, 36, 84, 126, 126, 84, 36, 9, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 2, 1, 0, 0, 0, 0, 0, 0, 0],
+            [1, 3, 3, 1, 0, 0, 0, 0, 0, 0],
+            [1, 4, 6, 4, 1, 0, 0, 0, 0, 0],
+            [1, 5, 10, 10, 5, 1, 0, 0, 0, 0],
+            [1, 6, 15, 20, 15, 6, 1, 0, 0, 0],
+            [1, 7, 21, 35, 35, 21, 7, 1, 0, 0],
+            [1, 8, 28, 56, 70, 56, 28, 8, 1, 0],
+            [1, 9, 36, 84, 126, 126, 84, 36, 9, 1],
         ];
         check_values(&ref_values, u32::comb);
         check_values(&ref_values, i32::comb);
