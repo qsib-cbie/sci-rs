c1d498595770ad1d0b5ffb520f42eef49f2f19c3
SpookyYomo <48710653+SpookyYomo@users.noreply.github.com>
2025-03-09 21:34:59 +0800

    Add Bessel as a trait: i0 for f64
    Implement bessel as a trait for f64 with the modified i0 bessel function
    first. Can easily be extended for Vec and Array as necessary later.

---
+++ sci-rs/src/special/bessel.rs
@@ -0,0 +1,14 @@
+/// All [functions located in the `Faster versions of common Bessel
+/// functions.`](<https://docs.scipy.org/doc/scipy/reference/special.html#faster-versions-of-common-bessel-functions>)
+pub trait Bessel {
+    /// Modified Bessel function of order 0.
+    ///
+    /// ## Notes
+    /// * The range is partitioned into the two intervals [0, 8] and (8, infinity).
+    /// * [Scipy has this as a
+    ///   ufunc](<https://docs.scipy.org/doc/scipy/reference/special.html#special-functions-scipy-special>),
+    ///   as a supposed wrapper over the Cephes routine. We try to define it over reasonable types in
+    ///   the impl.
+    ///
+    fn i0(&self) -> Self;
+}
--- sci-rs/src/special/xsf/mod.rs
+++ sci-rs/src/special/xsf/mod.rs
@@ -1,2 +1,4 @@
 mod chbevl;
 pub(crate) use chbevl::*;
+
+mod i0;
