c8a676986e4140731a494078a4b21a80de499bae
SpookyYomo <48710653+SpookyYomo@users.noreply.github.com>
2025-03-09 21:34:59 +0800

    Add Bessel as a trait: i0 for f64
    Implement bessel as a trait for f64 with the modified i0 bessel function
    first. Can easily be extended for Vec and Array as necessary later.

--- sci-rs/src/special/mod.rs
+++ sci-rs/src/special/mod.rs
@@ -3,6 +3,10 @@ mod combinatorics;
 
 pub use combinatorics::*;
 
+/// Adds the [Bessel] trait.
+mod bessel;
+pub use bessel::Bessel;
+
 // Name is from special/xsf folder, which is Scipy has designated as X special functions (written
 // in C++) that are not exposed to Python. We keep these set of functions as being crate internal.
 pub(crate) mod xsf;
