c2619e2dcc5657949c8d3bfb1f3153153e384610
SpookyYomo <48710653+SpookyYomo@users.noreply.github.com>
2025-03-11 13:19:14 +0800

    Add error enums
    The error variants here should guide the user as to the wrong use of
    functions, instead of taking down the entire program with a panic, which
    is especially necessary in an embedded environment.

--- sci-rs-core/src/lib.rs
+++ sci-rs-core/src/lib.rs
@@ -4,3 +4,32 @@
 
 #[cfg(feature = "alloc")]
 extern crate alloc;
+
+use core::{error, fmt};
+
+/// Errors raised whilst running sci-rs.
+#[derive(Debug, PartialEq, Eq)]
+pub enum Error {
+    /// Argument parsed into function were invalid.
+    #[cfg(feature = "alloc")]
+    InvalidArg {
+        /// The invalid arg
+        arg: alloc::string::String,
+        /// Explaining why arg is invalid.
+        reason: alloc::string::String,
+    },
+    /// Two or more optional arguments passed into functions conflict.
+    #[cfg(feature = "alloc")]
+    ConfictArg {
+        /// Explaining what arg is invalid.
+        reason: alloc::string::String,
+    },
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        todo!()
+    }
+}
+
+impl error::Error for Error {}
